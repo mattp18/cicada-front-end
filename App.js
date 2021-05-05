6eb565062795604ce7e8b00add5b5ee058f9826d
Matt <mpuent18@gmail.com>
2021-05-05 17:44:28 -0400

    updated App.js and created header component

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
+import CicadaItem from './components/cicadaitem'
 
 export default function App() {
+
+  const [cicadas, setCicadas] = useState([
+    { text: 'Cicada 1', key: '1' },
+    { text: 'Cicada 2' , key: '2' },
+    { text: 'Cicada 3' , key: '3' },
+    { text: 'Cicada 4' , key: '4' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/** header */}
+      <Header />
+      <View style={styles.content}>
+        {/** cicada form */}
+        <View style={styles.list}>
+          <FlatList
+            data={cicadas}
+            renderItem={({ item }) => (
+              <CicadaItem item={item} />
+            )}
+          />
+        </View>
+      </View>
+
     </View>
+    
+    
   );
 }
 
@@ -15,7 +38,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
